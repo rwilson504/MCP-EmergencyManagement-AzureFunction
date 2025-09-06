131a1915997820ff43bdec2c2347bca54d5101b7
ricwilson <richard.a.wilson@microsoft.com>
2025-09-06 13:57:02 -0400

    Add Required attribute to McpToolProperty fields

--- src/Common/McpToolProperty.cs
+++ src/Common/McpToolProperty.cs
@@ -2,8 +2,12 @@ namespace EmergencyManagementMCP.Common
 {
     public class McpToolProperty
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Type { get; set; }
+        [Required]
         public string Description { get; set; }
+        public bool Required { get; set; } = false;
     }
 }
